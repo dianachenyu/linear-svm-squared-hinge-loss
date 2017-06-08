d97d8a44a360c4b950942987f720c6d41daa5eb6
Diana Zhang <dczhang@uw.edu>
2017-06-07 23:17:39 -0700

    Change file name

---
+++ src/demo_compare.py
@@ -0,0 +1,25 @@
+"""
+Demo of the comparision of this implementaion and scikit-learn
+"""
+
+# Author: Diana Chenyu Zhang <dczhang@uw.edu>
+
+import numpy as np
+import pandas as pd
+from compare_to_sklearn import compare
+
+# Comparision on a simulated dataset
+features = np.zeros((60, 50))
+features[0:30, :] = np.random.normal(scale=1, size=(30, 50))
+features[30:60, :] = np.random.normal(loc=1, scale=5, size=(30, 50))
+labels = np.asarray([1]*30 + [-1]*30)
+print('Comparison on a simulated dataset: ')
+error_train_ipl, error_test_ipl, error_train_svm, error_test_svm = compare(features,labels)
+
+
+# Comparision on a real-world dataset
+spam = pd.read_table('https://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/spam.data', sep=' ', header=None)
+x = np.asarray(spam)[:, 0:-1]
+y = np.asarray(spam)[:, -1]*2 - 1 
+print('Comparison on a real-world dataset: ')
+error_train_ipl2, error_test_ipl2, error_train_svm2, error_test_svm2 = compare(x,y)
